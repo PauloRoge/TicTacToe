d26f5c311608f70910928b1bf8ad564af73c7f1e
paulo <paulocandido815@gmail.com>
2023-07-20 22:23:30 -0300

    help

--- jogodavelha/main.c
+++ jogodavelha/main.c
@@ -2,225 +2,146 @@
 #include <stdlib.h>
 #include <locale.h>
 #include <conio.h>
+#include <time.h> //para sortear números pseudoaleatórios.
 
-#define MAX_SIZE 10
+#define MAX_SIZE 3
+    //prototypes
 
-char continuar;
-int i, ind, fim;
-char teclado;
-int indice=0,vez=0;
+void info(); //imprime instruções iniciais.
+void show(char board[][MAX_SIZE]); //imprime a tabela em cada instância da partida.
+int fillBoard(char(*board)[3], char teclado, char marcador);  //atualiza as marcações no tabuleiro.
+int verificarVencedor(char jogo[][3]);
 
-void tecla(); //converte caracter em inteiro.
-void info(); //imprime instruções.
-void imprimir(char* wjogo); //imprime a tabela em cada instância.
+int main()
+{
+    setlocale(LC_ALL, "portuguese");
+    srand((unsigned int)time(NULL));
 
-int main(){
-    setlocale(LC_ALL,"portuguese");
+    char continuar; // armazena uma caracter que codifica uma condição de parar ou não parar.
+    char tecla;
+    int vez = rand() % 2;
+
+    char board[MAX_SIZE][MAX_SIZE]; //inicializa tabuleiro com (3x3) posições.
+
+    for (int i = 0; i < MAX_SIZE; i++) //atribui um backspace em cada posição vazia.
+        for (int j = 0; j < MAX_SIZE; j++)
+            board[i][j] = ' ';
+
     info();
-
-    do{
-        continuar=_getch();
-    } while (continuar!='s' && continuar!='n'); //garante que não haja entrada inválida.
-
-    while (continuar=='s')
+    while (continuar == 's')
     {
-        char jogo[MAX_SIZE];
-        for (int j = 0; j < MAX_SIZE; j++, jogo[j] = ' ');
-
-        for (i = 0; i < MAX_SIZE; i++)
+        do
         {
             system("cls");
-            imprimir(jogo);
+            show(board);
 
-            if (jogo[7]=='x' && jogo[4]=='x' && jogo[1]=='x')
-            {
-                printf("x venceu na primeira coluna.");
-                break;
-            }
-            if (jogo[8]=='x' && jogo[5]=='x' && jogo[2]=='x')
-            {
-                printf("x venceu na segunda coluna.");
-                break;
-            }
-            if (jogo[9]=='x' && jogo[6]=='x' && jogo[3]=='x')
-            {
-                printf("x venceu na terceira coluna.");
-                break;
-            }
-            if (jogo[7]=='x' && jogo[8]=='x' && jogo[9]=='x')
-            {
-                printf("x venceu  na terceira linha");
-                break;
-            }
-            if (jogo[4]=='x' && jogo[5]=='x' && jogo[6]=='x')
-            {
-                printf("x venceu na segunda linha.");
-                break;
-            }
-            if (jogo[1]=='x' && jogo[2]=='x' && jogo[3]=='x')
-            {
-                printf("x venceu na primeira linha.");
-                break;
-            }
-            if (jogo[7]=='x' && jogo[5]=='x' && jogo[3]=='x')
-            {
-                printf("x venceu na diagonal.");
-                break;
-            }
-            if (jogo[1]=='x' && jogo[5]=='x' && jogo[9]=='x')
-            {
-                printf("x venceu na diagonal secundária.");
-                break;
-            }
+            printf("vez de %s", vez == 0 ? "X" : "O");
+            tecla = _getch();
 
-            if (jogo[7]=='O' && jogo[4]=='O' && jogo[1]=='O')
-            {
-                printf("(o) venceu na primeira coluna.");
-                break;
-            }
-            if (jogo[8]=='O' && jogo[5]=='O' && jogo[2]=='O')
-            {
-                printf("(o) venceu na segunda coluna.");
-                break;
-            }
-            if (jogo[9]=='O' && jogo[6]=='O' && jogo[3]=='O')
-            {
-                printf("(o) venceu na terceira coluna.");
-                break;
-            }
-            if (jogo[7]=='O' && jogo[8]=='O' && jogo[9]=='O')
-            {
-                printf("(o) venceu na terceira linha.");
-                break;
-            }
-            if (jogo[4]=='O' && jogo[5]=='O' && jogo[6]=='O')
-            {
-                printf("(o) venceu na segunda linha.");
-                break;
-            }
-            if (jogo[1]=='O' && jogo[2]=='O' && jogo[3]=='O')
-            {
-                printf("(o) venceu na primeira linha.");
-                break;
-            }
-            if (jogo[7]=='O' && jogo[5]=='O' && jogo[3]=='O')
-            {
-                printf("(o) venceu na diagonal.");
-                break;
-            }
-            if (jogo[9]=='O' && jogo[5]=='O' && jogo[1]=='O')
-            {
-                printf("(o) venceu na diagonal secundária.");
-                break;
-            }
-             if(fim==45)
-            {
-                //system("color 7C");
-                printf("VELHA!!");
-                break;
-            }
-            if (indice%2 == 0)
-            {
-                printf(" vez do xis(x)\n\n");
-                do
-                {
-                    teclado=_getch();
-                    tecla(teclado);
+            vez = fillBoard(board, tecla, vez == 0 ? 'X' : 'O');
+            verificarVencedor(board);
 
-                    if (jogo[ind] == 'x'|| jogo[ind] =='O')
-                    {
-                        ind = 10; //para fazer novamente o laço.
-                    }
-                } while (ind < 1 || ind > 9);
+        } while (!verificarVencedor(board));
 
-                jogo[ind]='x';
-                fim=fim+ind;
-            }
-            if (indice%2 != 0)
-            {
-                printf(" vez da bolinha(o)\n\n");
-                do{
-                    teclado=_getch();
-                    tecla();
-                    if (jogo[ind] == 'O'|| jogo[ind] == 'x')
-                    {
-                        ind = 10; //para fazer novamente o laço.
-                    }
-                } while (ind < 1 || ind > 9);
-                jogo[ind]='O';
-                fim=fim+ind;
-            }
-            ind=0;
-            indice++;
-        }
-        fim=0;
-        vez++;
-        vez%2 != 0 ? indice= 1 : 0;
         printf("\n\n\tjogar novamente?\n\t(s)sim  (n)não");
-        
-        do{
-        continuar=_getch();
-        } while (continuar!='s' && continuar!='n');
-    } 
+        do
+        {
+            continuar = _getch();
+        } while (continuar != 's' && continuar != 'n'); //garante que não haja entrada inválida.
+    }
 }
 
-void tecla()
+int fillBoard(char(*board)[3], char teclado, char marcador)
 {
     switch (teclado)
     {
     case '1':
-        ind=1;
+        board[2][2] = marcador;
         break;
 
     case '2':
-        ind=2;
+        board[2][1] = marcador;
         break;
 
     case '3':
-        ind=3;
+        board[2][0] = marcador;
         break;
 
     case '4':
-        ind=4;
+        board[1][2] = marcador;
         break;
 
     case '5':
-        ind=5;
+        board[1][1] = marcador;
         break;
 
     case '6':
-        ind=6;
+        board[1][0] = marcador;
         break;
 
     case '7':
-        ind=7;
+        board[0][2] = marcador;
         break;
 
     case '8':
-        ind=8;
+        board[0][1] = marcador;
         break;
 
     case '9':
-        ind=9;
+        board[0][0] = marcador;
         break;
-    } 
+
+    default:
+        break;
+    }
+    if (marcador == 'X') //retorna a vez do próximo jogador.
+        return 1;
+    else
+        return 0;
 }
 
+//functions
 void info() {
     printf("\n\tJogo da velha. #1\n\n\tComo jogar: \n\n");
     printf("\t     |     |     \n\t  7  |  8  |  9  \n\t_____|_____|_____");
-    printf("\n\t     |     |     \t*use o teclado numérico para seleciona");
-    printf("r\n\t  4  |  5  |  6  \ta posição desejada.\n\t_____|_____|__");
+    printf("\n\t     |     |     \t*use o teclado numÃ©rico para seleciona");
+    printf("r\n\t  4  |  5  |  6  \ta posiÃ§Ã£o desejada.\n\t_____|_____|__");
     printf("___\n\t     |     |     \n\t  1  |  2  |  3  \n\t     |     |");
-    printf("     \n\n\tJogar? (s)sim (n)não");
+    printf("     \n\n\tJogar? (s)sim (n)nÃ£o");
 }
 
-void imprimir(char* wjogo) {
+void show(char board[][MAX_SIZE]) {
     printf("\n\n\n\n\n\n\n\n\n\t\t\t\t\t\t     |     |     \n");
-    printf("\t\t\t\t\t\t  %c  |  %c  |  %c  \n", wjogo[7], wjogo[8], wjogo[9]);
+    printf("\t\t\t\t\t\t  %c  |  %c  |  %c  \n", board[0][2], board[0][1], board[0][0]);
     printf("\t\t\t\t\t\t_____|_____|_____\n\t\t\t\t\t\t     |     |     \n");
-    printf("\t\t\t\t\t\t  %c  |  %c  |  %c  \n", wjogo[4], wjogo[5], wjogo[6]);
+    printf("\t\t\t\t\t\t  %c  |  %c  |  %c  \n", board[1][2], board[1][1], board[1][0]);
     printf("\t\t\t\t\t\t_____|_____|_____\n\t\t\t\t\t\t     |     |     \n");
-    printf("\t\t\t\t\t\t  %c  |  %c  |  %c  \n", wjogo[1], wjogo[2], wjogo[3]);
+    printf("\t\t\t\t\t\t  %c  |  %c  |  %c  \n", board[2][2], board[2][1], board[2][0]);
     printf("\t\t\t\t\t\t     |     |     \n");
+}
+
+    int verificarVencedor(char board[][3]) {
+    // Verificar linhas
+    for (int i = 0; i < 3; i++) {
+        if (board[i][0] == board[i][1] && board[i][0] == board[i][2]) {
+            return board[i][0];  // Retorna o jogador vencedor (x ou o)
+        }
+    }
+
+    // Verificar colunas
+    for (int j = 0; j < 3; j++) {
+        if (board[0][j] == board[1][j] && board[0][j] == board[2][j]) {
+            return board[0][j];  // Retorna o jogador vencedor (x ou o)
+        }
+    }
+
+    // Verificar diagonais
+    if (board[0][0] == board[1][1] && board[0][0] == board[2][2]) {
+        return board[0][0];  // Retorna o jogador vencedor (x ou o)
+    }
+    if (board[0][2] == board[1][1] && board[0][2] == board[2][0]) {
+        return board[0][2];  // Retorna o jogador vencedor (x ou o)
+    }
+
+    return 0;  // Retorna um espaço em branco se não houver vencedor ainda
 }
